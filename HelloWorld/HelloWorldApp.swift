02c144e35c76bf6a9e0ffb79633db83300beeda8
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-25 18:50:46 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Tacettin Küstü on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
