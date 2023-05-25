157ec407b7a9b316443b20272a6a5d2f4d3c03f9
Tacettin Kustu <tacettinkustu@gmail.com>
2023-05-25 21:05:26 +0300

    Display image and icon with modifiers

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -7,13 +7,42 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
 
     var body: some View {
+        // MARK: 1. Basic
         
         // AsyncImage(url: URL(string: imageURL))
-        AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: 2. Scale
+        
+        // AsyncImage(url: URL(string: imageURL), scale: 3.0)
+        
+        // MARK: 3. Placeholder
+        
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image.imageModifier()
+        } placeholder: {
+            Image(systemName: "photo.circle.fill").iconModifier()
+        }
+        .padding(40)
         
     }
 }
